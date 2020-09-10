84720f9fc3e9d71d9d0885b2a15bb1ec3bec30c3
Ubuntu <nico@deepstream5.u2olidavwwauzjkizbmjmbnmtb.cx.internal.cloudapp.net>
2020-09-10 16:38:53 +0000

    Add pyds module

--- utilities/video-analysis/dlstreamer/app/start.sh
+++ utilities/video-analysis/dlstreamer/app/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 runsvdir /var/runit &
 export GST_DEBUG=4
+# Copy nvidia pyds module
+cp /opt/nvidia/deepstream/deepstream-5.0/lib/pyds.so /app
+cp /opt/nvidia/deepstream/deepstream-5.0/lib/setup.py /app
+python3 setup.py
 python3 main.py -p 5001
